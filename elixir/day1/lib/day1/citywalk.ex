8b79639086db62b0838df85c15ff25c669e27b8c
Stephen Ball <sdball@gmail.com>
2016-12-07 22:36:58 -0500

    Add Citywalk and tests

---
+++ elixir/day1/lib/day1/citywalk.ex
@@ -0,0 +1,77 @@
+defmodule Day1.Citywalk do
+  use GenServer
+
+  defmodule State do
+    defstruct x: 0, y: 0, direction: :north
+  end
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def position(pid) do
+    GenServer.call(pid, :position)
+  end
+
+  def distance_from_start(pid) do
+    GenServer.call(pid, :distance_from_start)
+  end
+
+  def direction(pid) do
+    GenServer.call(pid, :direction)
+  end
+
+  def turn(pid, direction) do
+    GenServer.call(pid, {:turn, direction})
+  end
+
+  def walk(pid, blocks) do
+    GenServer.call(pid, {:walk, blocks})
+  end
+
+  def init([]) do
+    {:ok, %State{}}
+  end
+
+  def handle_call(:position, _from, %{x: x, y: y} = state) do
+    {:reply, [x,y], state}
+  end
+
+  def handle_call(:distance_from_start, _from, %{x: x, y: y} = state) do
+    {:reply, abs(x + y), state}
+  end
+
+  def handle_call(:direction, _from, %{direction: direction} = state) do
+    {:reply, direction, state}
+  end
+
+  def handle_call({:turn, :right}, _from, %{direction: direction} = state) do
+    new_direction = case direction do
+      :north -> :east
+      :east  -> :south
+      :south -> :west
+      :west  -> :north
+    end
+    {:reply, new_direction, %{state | direction: new_direction}}
+  end
+
+  def handle_call({:turn, :left}, _from, %{direction: direction} = state) do
+    new_direction = case direction do
+      :north -> :west
+      :west  -> :south
+      :south -> :east
+      :east  -> :north
+    end
+    {:reply, new_direction, %{state | direction: new_direction}}
+  end
+
+  def handle_call({:walk, blocks}, _from, %{x: x, y: y, direction: direction} = state) do
+    [x,y] = case direction do
+      :north -> [x, y + blocks]
+      :east  -> [x + blocks, y]
+      :south -> [x, y - blocks]
+      :west  -> [x - blocks, y]
+    end
+    {:reply, [x,y], %{state | x: x, y: y}}
+  end
+end
---
+++ elixir/day1/test/day1/citywalk_test.exs
@@ -0,0 +1,113 @@
+defmodule Day1.CitywalkTest do
+  use ExUnit.Case
+  alias Day1.Citywalk
+
+  setup do
+    {:ok, pid} = Citywalk.start_link
+    {:ok, [pid: pid]}
+  end
+
+  test "starts at position 0,0", %{pid: pid} do
+    assert Citywalk.position(pid) == [0,0]
+  end
+
+  test "starts at distance 0", %{pid: pid} do
+    assert Citywalk.distance_from_start(pid) == 0
+  end
+
+  test "starts facing north", %{pid: pid} do
+    assert Citywalk.direction(pid) == :north
+  end
+
+  test "can turn right", %{pid: pid} do
+    assert Enum.map(1..4, fn(_) ->
+      Citywalk.turn(pid, :right)
+      Citywalk.direction(pid)
+    end) == [ :east, :south, :west, :north ]
+  end
+
+  test "can turn left", %{pid: pid} do
+    assert Enum.map(1..4, fn(_) ->
+      Citywalk.turn(pid, :left)
+      Citywalk.direction(pid)
+    end) == [ :west, :south, :east, :north ]
+  end
+
+  test "can walk one block facing north", %{pid: pid} do
+    assert Citywalk.direction(pid) == :north
+    Citywalk.walk(pid, 1)
+    assert Citywalk.position(pid) == [0,1]
+  end
+
+  test "can walk one block facing east", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    assert Citywalk.direction(pid) == :east
+    Citywalk.walk(pid, 1)
+    assert Citywalk.position(pid) == [1,0]
+  end
+
+  test "can walk one block facing south", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    Citywalk.turn(pid, :right)
+    assert Citywalk.direction(pid) == :south
+    Citywalk.walk(pid, 1)
+    assert Citywalk.position(pid) == [0,-1]
+  end
+
+  test "can walk one block facing west", %{pid: pid} do
+    Citywalk.turn(pid, :left)
+    assert Citywalk.direction(pid) == :west
+    Citywalk.walk(pid, 1)
+    assert Citywalk.position(pid) == [-1,0]
+  end
+
+  test "can calculate distance from start going north", %{pid: pid} do
+    assert Citywalk.direction(pid) == :north
+    Citywalk.walk(pid, 5)
+    assert Citywalk.distance_from_start(pid) == 5
+  end
+
+  test "can calculate distance from start going east", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    assert Citywalk.direction(pid) == :east
+    Citywalk.walk(pid, 5)
+    assert Citywalk.distance_from_start(pid) == 5
+  end
+
+  test "can calculate distance from start going south", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    Citywalk.turn(pid, :right)
+    assert Citywalk.direction(pid) == :south
+    Citywalk.walk(pid, 5)
+    assert Citywalk.distance_from_start(pid) == 5
+  end
+
+  test "can calculate distance from start going west", %{pid: pid} do
+    Citywalk.turn(pid, :left)
+    assert Citywalk.direction(pid) == :west
+    Citywalk.walk(pid, 5)
+    assert Citywalk.distance_from_start(pid) == 5
+  end
+
+  test "can calculate a walk ending northeast", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 5)
+    Citywalk.turn(pid, :left)
+    Citywalk.walk(pid, 5)
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 5)
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 3)
+    assert Citywalk.distance_from_start(pid) == 12
+  end
+
+  test "can calculate a walk ending southwest", %{pid: pid} do
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 2)
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 2)
+    Citywalk.turn(pid, :right)
+    Citywalk.walk(pid, 3)
+    assert Citywalk.distance_from_start(pid) == 3
+  end
+end
